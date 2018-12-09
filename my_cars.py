d458ae9d654ec0ec5634e3de2375336b393cbdb2
HusterTian <40018115+HusterTian@users.noreply.github.com>
2018-12-09 21:22:56 +0800

    Initial commit

---
+++ my_cars.py
@@ -0,0 +1,8 @@
+from car import Car
+from electric_car import ElectricCar
+
+my_beetle = Car('volkswagen', 'bettle', 2016)
+print(my_beetle.get_desciptive_name())
+
+my_tesla = ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_desciptive_name())
