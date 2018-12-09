ae05d7f8cffcfdb06e4ea501d8520bfd5b2ad1e7
HusterTian <40018115+HusterTian@users.noreply.github.com>
2018-12-09 21:38:58 +0800

    sagiter
    sagiter

--- my_cars.py
+++ my_cars.py
@@ -6,3 +6,6 @@ print(my_beetle.get_desciptive_name())
 
 my_tesla = ElectricCar('tesla', 'roadster', 2016)
 print(my_tesla.get_desciptive_name())
+
+my_sagiter = Car('volkswagen', 'sagiter', 2015)
+print(my_sagiter.get_desciptive_name())
